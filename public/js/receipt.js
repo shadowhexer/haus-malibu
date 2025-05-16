8ddd8e4b2cb4bec3ee7a372e4df3770b32f914b2
shadowhexer <shadowhexer@yandex.com>
2025-05-16 20:15:30 +0800

    Emergency

--- public/js/receipt.js
+++ public/js/receipt.js
@@ -1,5 +1,3 @@
-import { jsPDF } from "jspdf";
-
 let receipt = [];
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -58,7 +56,7 @@ function displayReceipt() {
 };
 
 function generatePDF() {
-
+    const { jsPDF } = window.jspdf;
     const doc = new jsPDF();
     const rows = [booking].map(b => [
         b.book_id, b.first_name, b.last_name, b.checkIn, b.checkOut, b.date,
